f577c4ce435c2442543e65d340ff6a70895d8a42
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-30 15:21:29 -0700

    add UserContext, auth service, boilerplate Auth.jsx

---
+++ src/services/auth.js
@@ -0,0 +1,21 @@
+import { client } from './client';
+
+// get current user from supabase client
+export function getUser() {
+  return client.auth.currentUser;
+}
+
+export async function authUser(email, password, type) {
+  let response;
+  // check the url param following auth/ and tell the client what to do with the email/password
+  if (type === 'sign-up') {
+    response = await client.auth.signUp({ email, password });
+  } else {
+    response = await client.auth.signIn({ email, password });
+  }
+  if (response.error) {
+    throw response.error;
+  }
+  // return the signed in user
+  return response.user;
+}
