a463b334b2987d4417abbac69968a29782895df7
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-31 16:31:46 -0700

    add no blank task submissions, auth users can complete tasks

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,12 +1,32 @@
 import { useTasksContext } from '../../context/TasksContext';
+import { toggleTaskCompleted } from '../../services/tasks';
 
 export default function TaskList() {
   const { tasks, setTasks } = useTasksContext();
 
+  const handleCompleted = async (task) => {
+    try {
+      const completedTask = await toggleTaskCompleted(task);
+      setTasks((prevTasks) =>
+        prevTasks.map((prevTask) => (prevTask.id === task.id ? completedTask : prevTask))
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <>
       {tasks.map((task) => (
-        <div key={task.id}>{task.description}</div>
+        <div key={task.id}>
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => handleCompleted(task)}
+            />
+          </label>
+          {task.description}
+        </div>
       ))}
     </>
   );
--- src/services/tasks.js
+++ src/services/tasks.js
@@ -7,7 +7,18 @@ export async function getTasks() {
 }
 
 export async function createTask(description) {
+  // if input is blank don't insert
+  if (!description) return;
   // insert a task into the current users list
   const response = await client.from('todos').insert([{ description }]).single();
+  return checkError(response);
+}
+
+export async function toggleTaskCompleted({ id, complete }) {
+  const response = await client
+    .from('todos')
+    .update({ complete: !complete })
+    .match({ id })
+    .single();
   return checkError(response);
 }
